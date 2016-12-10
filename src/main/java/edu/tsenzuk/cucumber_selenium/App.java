826c5975cba1190149c8e932bd7bf0744715042f
Tsenzuk <tsenzuk@gmail.com>
2016-12-10 23:09:50 +0200

    initial commit

---
+++ src/main/java/edu/tsenzuk/cucumber_selenium/App.java
@@ -0,0 +1,13 @@
+package edu.tsenzuk.cucumber_selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
